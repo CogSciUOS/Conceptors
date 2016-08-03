5378639cddafdfe34bd6265bc2c0b6fcaff3fb09
Patrick Faion <pfaion@uos.de>
2016-08-03 17:14:13 +0200

    adjusted path in deleteEmptyWave

--- src/deleteEmptyWave.py
+++ src/deleteEmptyWave.py
@@ -8,12 +8,12 @@ def delEmpty(path):
         for d in dirs:
             direc = os.path.join(root, d)
             delEmpty(direc)
-        
+
         for name in files:
             f = os.path.join(root, name)
             if os.path.getsize(f) < 1500:
                 print(f)
                 os.remove(f)
 
-path = os.path.abspath('D:/Data/Projects/StudyProject/syll')
+path = os.path.abspath('/Users/pfaion/Git/Conceptors/data/birddb/syll')
 delEmpty(path)
