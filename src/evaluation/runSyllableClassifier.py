53dff214f255e22d40a4ac610fc2eb50e373501b
Brian Lewis <keller@csh.rit.edu>
2016-09-01 11:54:24 +0200

    remove config parsing because we can't get it how we want it to work

--- src/evaluation/runSyllableClassifier.py
+++ src/evaluation/runSyllableClassifier.py
@@ -2,7 +2,7 @@
 
 from matplotlib.pyplot import *
 import os
-import configargparse
+import argparse
 import pickle
 import sys
 
@@ -245,142 +245,136 @@ def runSyllClass(path='../../data/birddb/syll', syllN=5, trainN=30, cvalRuns=1, 
 
 """ argument parser """
 
-parser = configargparse.ArgumentParser(
-    description='Passes arguments on to syllable Classifier function',
-    default_config_files=['default.ini']
+parser = argparse.ArgumentParser(
+    description='Passes arguments on to syllable Classifier function'
 )
-#parser.add('-c', '--my-config', required=True, is_config_file=True, help='config file path')
-
 parser.add_argument(
     '--path',
-    #default='../../data/birddb/syll',
+    default='../../data/birddb/syll',
     type=str,
     help='directory to the folder that includes syllable folders with wave data'
 )
 parser.add_argument(
     '--syllN',
     type=int,
-    #default=10,
+    default=10,
     help='number of syllables to include in train/test data'
 )
 parser.add_argument(
     '--trainN',
-    #default=30,
+    default=30,
     type=int,
     help='number of training samples to use for each syllable (default = 30)'
 )
 parser.add_argument(
     '--cvalRuns',
-    #default=2,
+    default=2,
     type=int,
     help='Number of cross validation runs with different training/test data splits (default = 1)'
 )
 parser.add_argument(
     '--sampRate',
-    #default=20000,
+    default=20000,
     type=int,
     help='Sampling Rate that raw data will be downsampled to (default = 20000)'
 )
 parser.add_argument(
     '--interpolType',
-    #default='mean',
+    default='mean',
     type=str,
     help='type of interpolation to be used for downsampling.'
 )
 parser.add_argument(
     '--mfccN',
-    #default=20,
+    default=20,
     type=int,
     help='Number of mel frequency cepstral coefficients to extract for each mel frame (default = 25, which is the maximum possible)'
 )
-parser.add(
+parser.add_argument(
     '--invCoeffOrder',
-    #default=True,
-    action='store_true',
+    default=True,
     help='Boolean, if true: Extract last n mfcc instead of first n (default = False)'
 )
 parser.add_argument(
     '--winsize',
-    #default=20,
+    default=20,
     type=int,
     help='Size of the time-window to be used for mfcc extraction in ms (default = 20)'
 )
 parser.add_argument(
     '--melFramesN',
-    #default=64,
+    default=64,
     type=int,
     help='Desired number of time bins for mfcc data (default = 64)'
 )
 parser.add_argument(
     '--smoothL',
-    #default=5,
+    default=5,
     type=int,
     help='Desired length of the smoothed mfcc data (default = 4)'
 )
 parser.add_argument(
     '--polyOrder',
-    #default=3,
+    default=3,
     type=int,
     help='Order of the polynomial used for mfcc data smoothing (default = 3)'
 )
 parser.add_argument(
     '--incDer',
-    #default=[True, True],
+    default=[True, True],
     type=list,
-    action='append',
     help='List of 2 booleans indicating whether to include 1./2. derivative of mfcc data or not (default = [True,True])'
 )
 parser.add_argument(
     '--resN',
-    #default=10,
+    default=10,
     type=int,
     help='Size of the reservoir to be used for conceptor learning (default = 10)'
 )
 parser.add_argument(
     '--specRad',
-    #default=1.1,
+    default=1.1,
     type=float,
     help='Spectral radius of the connectivity matrix of the reservoir (default = 1.2)'
 )
 parser.add_argument(
     '--biasScale',
-    #default=0.5,
+    default=0.5,
     type=float,
     help='Scaling of the bias term to be introduced to each reservoir element (default = 0.2)'
 )
 parser.add_argument(
     '--inpScale',
-    #default=1.0,
+    default=1.0,
     type=float,
     help='Scaling of the input of the reservoir (default = 1.0)'
 )
 parser.add_argument(
     '--conn',
-    #default=1.0,
+    default=1.0,
     type=float,
     help='Downscaling of the reservoir connections (default = 1.0)'
 )
 parser.add_argument(
     '--gammaPos',
-    #default=25,
+    default=25,
     type=int,
     help='Aperture to be used for computation of the positive conceptors'
 )
 parser.add_argument(
     '--gammaNeg',
-    #default=20,
+    default=20,
     type=int,
     help='Aperture to be used for computation of the negative conceptors'
 )
 parser.add_argument(
     '--plotExample',
-    #default=True,
-    action='store_true',
+    default=True,
     help='If true, plot raw & preprocessed mfcc data as well as conceptor evidences (default = False)'
 )
 parser.add_argument(
     '--targetDir',
-    #default=None,
+    default=None,
     type=str,
     help='Subdirectory in which results are to be stored'
 )
@@ -389,12 +383,9 @@ parser.add_argument(
 """ Run script via command window """
 # can be also run using an IDE, but uses the default parameters then
 try:
-    args = parser.parse()
-    #args = parser.parse_known_args()
-    #print(args['path'])
+    args = parser.parse_args()
     print(args)
 except:
-    print('A parameter was either missing or wrong')
     sys.exit(0)
 
 results = runSyllClass(args.path, args.syllN, args.trainN, args.cvalRuns, args.sampRate, args.interpolType,
